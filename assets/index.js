2dfa1b2ea82ee882c6039801f895e5c62e8c3c13
siemaszko41 <robicie.202@gmail.com>
2025-08-26 21:48:35 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,22 @@
-
 var selector = document.querySelector(".selector_box");
 selector.addEventListener('click', () => {
-    if (selector.classList.contains("selector_open")){
-        selector.classList.remove("selector_open")
-    }else{
-        selector.classList.add("selector_open")
-    }
-})
+    selector.classList.toggle("selector_open");
+});
 
 document.querySelectorAll(".date_input").forEach((element) => {
     element.addEventListener('click', () => {
-        document.querySelector(".date").classList.remove("error_shown")
-    })
-})
+        document.querySelector(".date").classList.remove("error_shown");
+    });
+});
 
-var sex = "m"
+var sex = "m";
 
 document.querySelectorAll(".selector_option").forEach((option) => {
     option.addEventListener('click', () => {
         sex = option.id;
         document.querySelector(".selected_text").innerHTML = option.innerHTML;
-    })
-})
+    });
+});
 
 var upload = document.querySelector(".upload");
 
@@ -30,17 +25,15 @@ imageInput.type = "file";
 imageInput.accept = ".jpeg,.png,.gif";
 
 document.querySelectorAll(".input_holder").forEach((element) => {
-
     var input = element.querySelector(".input");
     input.addEventListener('click', () => {
         element.classList.remove("error_shown");
-    })
-
+    });
 });
 
 upload.addEventListener('click', () => {
     imageInput.click();
-    upload.classList.remove("error_shown")
+    upload.classList.remove("error_shown");
 });
 
 imageInput.addEventListener("change", () => {
@@ -50,48 +43,33 @@ imageInput.addEventListener("change", () => {
 
     let file = imageInput.files[0];
     if (file) {
-        let url = URL.createObjectURL(file); // <-- zamiast fetch
+        let url = URL.createObjectURL(file); // 🔥 tu zamiast Imgur API
         upload.setAttribute("selected", url);
         upload.querySelector(".upload_uploaded").src = url;
     }
 });
 
-    .then(result => result.json())
-    .then(response => {
-        
-        var url = response.data.link;
-        upload.classList.remove("error_shown")
-        upload.setAttribute("selected", url);
-        upload.classList.add("upload_loaded");
-        upload.classList.remove("upload_loading");
-        upload.querySelector(".upload_uploaded").src = url;
-
-    })
-
-})
-
 document.querySelector(".go").addEventListener('click', () => {
-
     var empty = [];
-
     var params = new URLSearchParams();
 
-    params.set("sex", sex)
+    params.set("sex", sex);
+
     if (!upload.hasAttribute("selected")){
         empty.push(upload);
-        upload.classList.add("error_shown")
-    }else{
-        params.set("image", upload.getAttribute("selected"))
+        upload.classList.add("error_shown");
+    } else {
+        params.set("image", upload.getAttribute("selected"));
     }
 
     var birthday = "";
     var dateEmpty = false;
     document.querySelectorAll(".date_input").forEach((element) => {
-        birthday = birthday + "." + element.value
+        birthday = birthday + "." + element.value;
         if (isEmpty(element.value)){
             dateEmpty = true;
         }
-    })
+    });
 
     birthday = birthday.substring(1);
 
@@ -99,54 +77,38 @@ document.querySelector(".go").addEventListener('click', () => {
         var dateElement = document.querySelector(".date");
         dateElement.classList.add("error_shown");
         empty.push(dateElement);
-    }else{
-        params.set("birthday", birthday)
+    } else {
+        params.set("birthday", birthday);
     }
 
     document.querySelectorAll(".input_holder").forEach((element) => {
-
         var input = element.querySelector(".input");
 
         if (isEmpty(input.value)){
             empty.push(element);
             element.classList.add("error_shown");
-        }else{
-            params.set(input.id, input.value)
+        } else {
+            params.set(input.id, input.value);
         }
-
-    })
+    });
 
     if (empty.length != 0){
         empty[0].scrollIntoView();
-    }else{
-
+    } else {
         forwardToId(params);
     }
-
 });
 
 function isEmpty(value){
-
-    let pattern = /^\s*$/
+    let pattern = /^\s*$/;
     return pattern.test(value);
-
 }
 
 function forwardToId(params){
-
-    location.href = "/xsad/id?" + params
-
+    location.href = "/xsad/id?" + params;
 }
 
 var guide = document.querySelector(".guide_holder");
 guide.addEventListener('click', () => {
-
-    if (guide.classList.contains("unfolded")){
-        guide.classList.remove("unfolded");
-    }else{
-        guide.classList.add("unfolded");
-    }
-
-})
-
-
+    guide.classList.toggle("unfolded");
+});
