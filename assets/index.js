cf5e2c2535a5de8dd0cdb11fea14f82551498af2
siemaszko41 <robicie.202@gmail.com>
2025-08-26 21:34:14 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -54,13 +54,13 @@ imageInput.addEventListener('change', (event) => {
     var data = new FormData();
     data.append("image", file);
 
-    fetch('	https://api.imgur.com/3/image' ,{
-        method: 'POST',
-        headers: {
-            'Authorization': 'Client-ID ec67bcef2e19c08'
-        },
-        body: data
-    })
+    fetch("https://api.imgur.com/3/image", {
+    method: "POST",
+    headers: {
+        "Authorization": "Client-ID ec67bcef2e19c08"
+    },
+    body: data
+})
     .then(result => result.json())
     .then(response => {
         
@@ -153,3 +153,4 @@ guide.addEventListener('click', () => {
     }
 
 })
+
